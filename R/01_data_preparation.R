f51ece719f63281f05d522ac84c3464ee57c5725
KunzstLD <sk193@web.de>
2021-02-05 18:16:58 +0100

    05.02: Fixed data prep

--- R/01_data_preparation.R
+++ R/01_data_preparation.R
@@ -1,33 +1,96 @@
-# ______________________________________________________________
-# Data preparation 
+# __________________________________________________________________________
+# Data preparation
 # TODO: col_two_levels actually selects also for columns with less than
 # two levels
-# ______________________________________________________________
+# ___________________________________________________________________________
 
-# read in data
-# data <- readRDS(file.path(data_in, "Trait_AUS_agg.rds"))
+# Load trait data
+trait_data_ww <- load_data(pattern = "*.rds", path = data_in)
 
-# analyse base way -> data table does not support row.names
-setDF(data)
+# Check if trait datasets have the same colnames
+trait_data_ww %>% check_colNames()
 
-# turn cols into ordered factors
-# trait_cols <- names(data)[!names(data) %like% "order|family"]
-# data[, trait_cols] <- lapply(data[, trait_cols], as.ordered)
+# Rm dev for now
+trait_data_ww <- lapply(
+  trait_data_ww,
+  function(y) y[, c("dev_holometabol", "dev_hemimetabol") := NULL]
+)
 
-# data with only two levels have to be numeric 
-col_two_levels <- data[, -grep("order|family", names(data))] %>%
-  lapply(levels) %>%
-  lapply(., function(y)
-    length(y)[length(y) <= 2]) %>%
-  unlist %>%
-  names
+# rename file list
+names(trait_data_ww) <- sub(
+  "([A-z]{5})(\\_)([A-z]{2,})(\\_)([A-z]{3})(.+)",
+  "\\3",
+  names(trait_data_ww)
+)
 
-if(length(col_two_levels) == 1) {
-  data[[col_two_levels]] <- sapply(data[[col_two_levels]], as.double)
+# Arrange colnames according to traits
+lapply(
+  trait_data_ww,
+  function(y) {
+    setcolorder(y, c(
+      "family",
+      "feed_shredder",
+      "feed_gatherer",
+      "feed_filter",
+      "feed_predator",
+      "feed_herbivore",
+      "feed_parasite",
+      "resp_teg",
+      "resp_gil",
+      "resp_pls_spi",
+      "volt_semi",
+      "volt_uni",
+      "volt_bi_multi",
+      "locom_swim",
+      "locom_crawl",
+      "locom_burrow",
+      "locom_sessil",
+      "ovip_ter",
+      "ovip_ovo",
+      "ovip_aqu",
+      "size_medium",
+      "size_small",
+      "size_large",
+      "bf_streamlined",
+      "bf_flattened",
+      "bf_cylindrical",
+      "bf_spherical"
+    ))
+  }
+)
+
+# ---- Data preparation for HC -----------------------------------------------
+
+preproc_traits <- list()
+for (region in c("AUS", "EU", "NOA", "NZ")) {
+  data <- trait_data_ww[[region]]
+  data[, order := NULL]
+
+  # convert to data.frame -> data table does not support row.names
+  setDF(data)
+
+  # add row.names
+  row.names(data) <- data$family
+  data$family <- NULL
+
+  # data with only two levels/integer/factor variables have to be numeric
+  col_int_fac <-
+    names(Filter(function(y) {
+      is.integer(y) | is.factor(y)
+    }, data))
+  data[, col_int_fac] <- apply(data[, col_int_fac], 2, as.double)
+
+  # Convert to ktab object
+  vec <- sub("\\_.*", "\\1", names(data))
+  blocks <- rle(vec)$lengths
+  data <- prep.fuzzy(data, blocks)
+  data <- ktab.list.df(list(data))
+
+  # save to list
+  preproc_traits[[region]] <- data
 }
-if (length(col_two_levels) > 1) {
-  data[, col_two_levels] <- lapply(data[, col_two_levels], as.double)
-}
 
-# add row.names
-row.names(data) <- data$family
+saveRDS(
+  object = preproc_traits,
+  file = file.path(data_cache, "preproc_traits.rds")
+)
